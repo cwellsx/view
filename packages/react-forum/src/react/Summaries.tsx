3f1b2c4bfcd5f3a3a1e70008c1608517feacfcca
cwellsx <cwellsx@gmail.com>
2019-05-04 17:59:16 +0200

    Save current user in Context and show gravatar

--- packages/react-forum/src/react/Summaries.tsx
+++ packages/react-forum/src/react/Summaries.tsx
@@ -1,15 +1,27 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import * as I from "../data";
 import { Content } from './Column';
-import * as Links from './Links';
+import { getPageUrl } from "../io/pageId";
 
-export function getImageSummary(x: I.ImageSummary): Content {
+export function getImageSummary(summary: I.ImageSummary): Content {
+  const href = getPageUrl({ pageType: "Image", id: summary.idName });
   const element: React.ReactElement = (
     <React.Fragment>
-      <h3>{Links.image(x.idName)}</h3>
-      <p>{x.summary}</p>
+      <h3><NavLink to={href}>{summary.idName.name}</NavLink></h3>
+      <p>{summary.summary}</p>
     </React.Fragment>
   );
-  const key = `/images/${x.idName.id}`;
-  return { element, key };
+  return { element, key: href };
+}
+
+export function getUserName(idName: I.IdName): React.ReactElement {
+  const href = getPageUrl({ pageType: "User", id: idName });
+  return <NavLink to={href}>{idName.name}</NavLink>;
+}
+
+export function getUserSmallGravatar(summary: I.UserSummary, title: boolean = true): React.ReactElement {
+  const href = getPageUrl({ pageType: "User", id: summary.idName });
+  const src = `https://www.gravatar.com/avatar/${summary.gravatarHash}?s=48&d=identicon&r=PG`;
+  return <NavLink to={href} title={title ? summary.idName.name : undefined}><img src={src} className="gravatar-small" alt={summary.idName.name} width="24" height="24" /></NavLink>;
 }
